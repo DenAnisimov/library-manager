4d74f3a5fe2dc564330ccb34601a5aad85fc6052
DenAnisimov <den.anisimov.2206@mail.ru>
2024-07-13 01:43:51 +0300

    DTOs

---
+++ src/main/java/org/example/dto/BookDTO.java
@@ -0,0 +1,60 @@
+package org.example.dto;
+
+import java.time.LocalDate;
+
+public class BookDTO {
+    private int id;
+    private String title;
+    private String description;
+    private int authorId;
+    private LocalDate publicationDate;
+
+    public BookDTO(int id, String title, String description, int authorId, LocalDate publicationDate) {
+
+        this.id = id;
+        this.title = title;
+        this.description = description;
+        this.authorId = authorId;
+        this.publicationDate = publicationDate;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getAuthorId() {
+        return authorId;
+    }
+
+    public void setAuthorId(int authorId) {
+        this.authorId = authorId;
+    }
+
+    public LocalDate getPublicationDate() {
+        return publicationDate;
+    }
+
+    public void setPublicationDate(LocalDate publicationDate) {
+        this.publicationDate = publicationDate;
+    }
+}
---
+++ src/main/java/org/example/dto/BookGenreDTO.java
@@ -0,0 +1,27 @@
+package org.example.dto;
+
+public class BookGenreDTO {
+    private int bookId;
+    private int genreId;
+
+    public BookGenreDTO(int bookId, int genreId) {
+        this.bookId = bookId;
+        this.genreId = genreId;
+    }
+
+    public int getBookId() {
+        return bookId;
+    }
+
+    public void setBookId(int bookId) {
+        this.bookId = bookId;
+    }
+
+    public int getGenreId() {
+        return genreId;
+    }
+
+    public void setGenreId(int genreId) {
+        this.genreId = genreId;
+    }
+}
