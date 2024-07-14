dd6fc5b4275c742b46ceaea1a0778960427f0654
DenAnisimov <den.anisimov.2206@mail.ru>
2024-07-14 16:28:55 +0300

    Рефактор кода

--- src/main/java/org/example/dao/AuthorDAOImpl.java
+++ src/main/java/org/example/dao/AuthorDAOImpl.java
@@ -6,18 +6,18 @@ import org.example.entity.Author;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
-import java.text.MessageFormat;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
 public class AuthorDAOImpl implements AuthorDAO {
 
     @Override
-    public List<Author> getAll() throws Exception {
-        try (Connection connection = new DataBaseConnection().getConnection()) {
-            String sql = "SELECT * FROM author";
+    public List<Author> getAll() throws SQLException {
+        String sql = "SELECT id, name FROM author";
 
-            PreparedStatement preparedStatement = connection.prepareStatement(sql);
+        try (Connection connection = new DataBaseConnection().getConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
             ResultSet resultSet = preparedStatement.executeQuery();
 
             List<Author> authors = new ArrayList<>();
@@ -33,11 +33,11 @@ public class AuthorDAOImpl implements AuthorDAO {
     }
 
     @Override
-    public Author getById(int id) throws Exception {
-        try (Connection connection = new DataBaseConnection().getConnection()) {
-            String sql = "SELECT * FROM author WHERE id = ?";
+    public Author getById(int id) throws SQLException {
+        String sql = "SELECT id, name FROM author WHERE id = ?";
 
-            PreparedStatement preparedStatement = connection.prepareStatement(sql);
+        try (Connection connection = new DataBaseConnection().getConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
             preparedStatement.setInt(1, id);
             ResultSet resultSet = preparedStatement.executeQuery();
 
@@ -53,20 +53,20 @@ public class AuthorDAOImpl implements AuthorDAO {
     }
 
     @Override
-    public void insert(Author entity) throws Exception {
-        try (Connection connection = new DataBaseConnection().getConnection()) {
-            String sql = "INSERT INTO author (name) VALUES(?)";
-            PreparedStatement preparedStatement = connection.prepareStatement(sql);
+    public void insert(Author entity) throws SQLException {
+        String sql = "INSERT INTO author (name) VALUES(?)";
+        try (Connection connection = new DataBaseConnection().getConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
             preparedStatement.setString(1, entity.getName());
             preparedStatement.execute();
         }
     }
 
     @Override
-    public void update(Author entity) throws Exception {
-        try (Connection connection = new DataBaseConnection().getConnection()) {
-            String sql = "UPDATE author SET name=? WHERE id=?";
-            PreparedStatement preparedStatement = connection.prepareStatement(sql);
+    public void update(Author entity) throws SQLException {
+        String sql = "UPDATE author SET name=? WHERE id=?";
+        try (Connection connection = new DataBaseConnection().getConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
             preparedStatement.setString(1, entity.getName());
             preparedStatement.setInt(2, entity.getId());
             preparedStatement.execute();
@@ -74,10 +74,10 @@ public class AuthorDAOImpl implements AuthorDAO {
     }
 
     @Override
-    public void delete(int id) throws Exception {
-        try (Connection connection = new DataBaseConnection().getConnection()) {
-            String sql = "DELETE FROM author WHERE id=?";
-            PreparedStatement preparedStatement = connection.prepareStatement(sql);
+    public void delete(int id) throws SQLException {
+        String sql = "DELETE FROM author WHERE id=?";
+        try (Connection connection = new DataBaseConnection().getConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
             preparedStatement.setInt(1, id);
             preparedStatement.execute();
         }
