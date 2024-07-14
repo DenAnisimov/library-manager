c8267a3f1ca64c5050eb08e9cd6752fb045fb9cf
DenAnisimov <den.anisimov.2206@mail.ru>
2024-07-14 16:27:58 +0300

    Изменил выбрасываемое исключение на SQLException

--- src/main/java/org/example/dao/CrudDAO.java
+++ src/main/java/org/example/dao/CrudDAO.java
@@ -1,15 +1,16 @@
 package org.example.dao;
 
+import java.sql.SQLException;
 import java.util.List;
 
 public interface CrudDAO<T> {
-    List<T> getAll() throws Exception;
+    List<T> getAll() throws SQLException;
 
-    T getById(int id) throws Exception;
+    T getById(int id) throws SQLException;
 
-    void insert(T entity) throws Exception;
+    void insert(T entity) throws SQLException;
 
-    void update(T entity) throws Exception;
+    void update(T entity) throws SQLException;
 
-    void delete(int id) throws Exception;
+    void delete(int id) throws SQLException;
 }
