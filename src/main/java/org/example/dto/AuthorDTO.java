bdc925c1ac851e79f114be6cc6a81eca6fc2d9b5
DenAnisimov <den.anisimov.2206@mail.ru>
2024-07-16 19:26:19 +0300

    Реализовал паттерн Builder и добавил методы equals и hashCode

--- src/main/java/org/example/dto/AuthorDTO.java
+++ src/main/java/org/example/dto/AuthorDTO.java
@@ -1,10 +1,17 @@
 package org.example.dto;
 
+import java.util.Objects;
+
 public class AuthorDTO {
     private int id;
     private String name;
 
     public AuthorDTO() {}
+
+    public AuthorDTO(Builder builder) {
+        this.id = builder.id;
+        this.name = builder.name;
+    }
 
     public AuthorDTO(int id, String name) {
         this.id = id;
@@ -25,5 +32,37 @@ public class AuthorDTO {
 
     public void setId(int id) {
         this.id = id;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof AuthorDTO authorDTO)) return false;
+        return id == authorDTO.id && Objects.equals(name, authorDTO.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name);
+    }
+
+    public static class Builder {
+        private int id;
+        private String name;
+
+        public Builder id(int id) {
+            this.id = id;
+            return this;
+        }
+
+        public Builder name(String name) {
+            this.name = name;
+            return this;
+        }
+
+        public AuthorDTO build() {
+            return new AuthorDTO(this);
+        }
+
     }
 }
